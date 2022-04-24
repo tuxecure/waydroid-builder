21278b6f912ccf7fef2f96d9fc485207e575430c
Rahammetoela Toekiman <fuseteam@duck.com>
2022-04-24 14:44:03 -0300

    upload time (~^_^)~

--- build-waydroid-for-debian-or-ubuntu-v4.sh
+++ build-waydroid-for-debian-or-ubuntu-v4.sh
@@ -35,8 +35,8 @@ do
   debuild -b -uc -us
   echo -e "${GREEN}==> Building package: ${i}, done.${NC}\n"
   echo -e "${GREEN}==> Installing packages ...${NC}"
-  ls
-  apt-get install -y ../*.deb && \
+  cd ..
+  apt-get install -y ./*.deb && \
   echo -e "${GREEN}==> Installing ${i} packages success${NC}"|| exit 1
   mv *.deb packages 2> /dev/null
   mv *.ddeb packages 2> /dev/null
